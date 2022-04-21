94318eadc5eba0fd77974933a316c91338f82799
Kyle Carlo <kyle29.zero@gmail.com>
2022-04-21 14:13:11 +0800

    initial commit

---
+++ ccdstru_mp.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    // SYSTEM VARIABLES
+    int bool_over = 0, //false
+        bool_ok = 0, //false
+        bool_aTurn = 1, //true
+        nAlpha[][2] = {
+                        {6,2},
+                        {6,4},
+                        {7,1},
+                        {7,3},
+                        {7,5}
+                    },
+        nBeta[][2] = {
+                        {1,1},
+                        {1,3},
+                        {1,5},
+                        {2,2},
+                        {2,4}
+                    };
+    // inputs
+    int nprev[2], nnext[2];
+    printf("Prev (a,b): ");
+    scanf("%d %d", nprev, nprev + 1);
+    printf("Next (c,d): ");
+    scanf("%d %d", nnext, nnext + 1);
+
+    if (bool_aTurn && )
+    {
+        /* code */
+    }
+        
+}
